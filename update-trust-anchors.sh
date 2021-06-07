a520c60f80e6ef2ff6efeff51c7899d45f4b0fe1
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2021-06-07 16:11:44 +0200

    Proper bash env variable expansion
    Kudos to @giacomini for spotting these bugs.

--- update-trust-anchors.sh
+++ update-trust-anchors.sh
@@ -25,15 +25,15 @@ DEST=/etc/pki/ca-trust/extracted
 /usr/bin/p11-kit extract --comment --format=pem-bundle --filter=ca-anchors --overwrite --purpose client-auth $DEST/pem/tls-ca-bundle-client.pem
 cat $DEST/pem/tls-ca-bundle.pem $DEST/pem/tls-ca-bundle-client.pem >> $DEST/pem/tls-ca-bundle-all.pem
 
-TRUST_ANCHORS_TARGET=${TRUST_ANCHORS_TARGET:-}
-CA_BUNDLE_TARGET=${CA_BUNDLE_TARGET:-}
+TRUST_ANCHORS_TARGET=${TRUST_ANCHORS_TARGET:=}
+CA_BUNDLE_TARGET=${CA_BUNDLE_TARGET:=}
 
-if [ -n ${TRUST_ANCHORS_TARGET} ]; then
+if [ -n "${TRUST_ANCHORS_TARGET}" ]; then
   echo "Copying trust anchors to ${TRUST_ANCHORS_TARGET}"
   rsync -avu -O --no-owner --no-group --no-perms /etc/grid-security/certificates/ ${TRUST_ANCHORS_TARGET}
 fi
 
-if [ -n ${CA_BUNDLE_TARGET} ]; then
+if [ -n "${CA_BUNDLE_TARGET}" ]; then
   echo "Copying ca bundle to ${CA_BUNDLE_TARGET}"
   rsync -avu -O --no-owner --no-group --no-perms --exclude 'CA/private'  /etc/pki/ ${CA_BUNDLE_TARGET}
 fi
